7767769aaf560beb368b87e227fc8a5cded9acd7
Fancy <maciekgplay@gmail.com>
2023-04-29 13:08:48 +0200

    Homepage desktop styling

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import CountryList from "../../components/CountryList/CountryList"
 import FilterSelector from "../../components/FilterSelector/FilterSelector"
 import SearchBar from "../../components/SearchBar/SearchBar"
+import './Home.scss'
 
 const Home = () => {
 
@@ -10,8 +11,10 @@ const Home = () => {
 
     return (
         <>
-            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-            <FilterSelector regionFilter={regionFilter} setRegionFilter={setRegionFilter}/>
+            <div className="search-filters-wrapper">
+                <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+                <FilterSelector regionFilter={regionFilter} setRegionFilter={setRegionFilter}/>
+            </div>
             <CountryList searchQuery={searchQuery} regionFilter={regionFilter}/>
         </>
     )
