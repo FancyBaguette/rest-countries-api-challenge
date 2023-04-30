8119def45b2a86617964498379747c64dfbcc4cc
Fancy <maciekgplay@gmail.com>
2023-04-30 18:50:05 +0200

    Country details desktop styling
    Rewrote the markup and made the whole thing responsive

--- src/views/CountryDetails/CountryDetails.jsx
+++ src/views/CountryDetails/CountryDetails.jsx
@@ -60,54 +60,59 @@ const CountryDetails = (props) => {
 
     return (
         countryObject && (
-            <div className={classNames('country-details', {'country-details--dark': isThemeDark})}>
+            <>
                 <Link 
                     className={classNames
                         ('nav-btn', 'go-back-btn', {'nav-btn--dark': isThemeDark}, {'go-back-btn--dark': isThemeDark})
                     } 
                     to='/'
                 >
-                    <p>Back</p>
+                    Back
                 </Link>
 
-                <img src={countryObject.flags.png} alt={`${countryObject.name.common} flag`} />
-
-                <h1>{countryObject.name.common}</h1>
-
-                <div className="country-details__data-block">
-                    <p><span>Native Names:</span> {countryNativeNames && formatNativeNames(countryNativeNames)}</p>
-                    <p><span>Population:</span> {countryObject.population}</p>
-                    <p><span>Region:</span> {countryObject.region}</p>
-                    <p><span>Sub Region:</span> {countryObject.subregion}</p>
-                    <p><span>Capital:</span> {countryObject.capital}</p>
-                </div>
-
-                <div className="country-details__data-block">
-                    <p><span>Top Level Domain:</span> {countryObject.tld}</p>
-                    <p><span>Currencies:</span> {getCurrencies(countryObject.currencies)}</p>
-                    <p><span>Languages:</span> {getLanguages(countryObject.languages)}</p>
-                </div>
-
-                { countryBorders &&
-                    <>
-                        <h2>Border Countries:</h2>
-                        <div className="borders-wrapper">
-                            {
-                                countryBorders.map(border => {
-                                    return (
-                                            <Link 
-                                                className={classNames('nav-btn', 'border-btn', {'nav-btn--dark': isThemeDark})} 
-                                                to={`/details/${border.code}`}
-                                            >
-                                                <p>{border.name}</p>
-                                            </Link>
-                                    )
-                                })
-                            }
+                <div className={classNames('country-details', {'country-details--dark': isThemeDark})}>
+                    <div className="country-details__flag-wrapper">
+                        <img className="country-details__flag" src={countryObject.flags.svg} alt={`${countryObject.name.common} flag`} />
+                    </div>
+                    <div className="country-details__data-wrapper">
+                        <h1 className="data-wrapper__country-title">{countryObject.name.common}</h1>
+                        <div className="data-wrapper__trivia-wrapper">
+                            <div className="trivia-wrapper__trivia-block">
+                                <p><span>Native Names:</span> {countryNativeNames && formatNativeNames(countryNativeNames)}</p>
+                                <p><span>Population:</span> {countryObject.population}</p>
+                                <p><span>Region:</span> {countryObject.region}</p>
+                                <p><span>Sub Region:</span> {countryObject.subregion}</p>
+                                <p><span>Capital:</span> {countryObject.capital}</p>
+                            </div>
+                            <div className="trivia-wrapper__trivia-block">
+                                <p><span>Top Level Domain:</span> {countryObject.tld}</p>
+                                <p><span>Currencies:</span> {getCurrencies(countryObject.currencies)}</p>
+                                <p><span>Languages:</span> {getLanguages(countryObject.languages)}</p>
+                            </div>
                         </div>
-                    </>
-                }
-            </div>
+                        { countryBorders &&
+                            <div className="data-wrapper__borders-wrapper">
+                                <h2>Border Countries:</h2>
+                                <div className="borders-wrapper__border-links">
+                                    {
+                                        countryBorders.map(border => {
+                                            return (
+                                                    <Link 
+                                                        className={classNames('nav-btn', 'border-btn', {'nav-btn--dark': isThemeDark})} 
+                                                        key={border.code}
+                                                        to={`/details/${border.code}`}
+                                                    >
+                                                        {border.name}
+                                                    </Link>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </>
         )
     )
 }
